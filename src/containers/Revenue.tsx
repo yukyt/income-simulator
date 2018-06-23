6dca0f0cb03d84b773e5bfe2566f0cce163cb47a
yuki <yuki@pridile.net>
2018-06-24 03:27:42 +0900

    労働形態に応じて収入を出し分け

--- src/containers/Revenue.tsx
+++ src/containers/Revenue.tsx
@@ -1,15 +1,30 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import Salary from '../components/Salary';
 import Sales from '../components/Sales';
+import * as CONSTANTS from '../define';
 
-const Revenue = ({
-  }) => (
-  <section>
-    <Salary />
-    <Sales />
-  </section>
-);
+interface IProps {
+  workStyle: number
+}
 
+class Revenue extends React.Component<IProps, {}> {
+  public render() {
+    return (
+      <section>
+        <div style={{ display: this.props.workStyle !== CONSTANTS.WORK_STYLE.SELF_EMPLOYEE ? '' : 'none' }}>
+          <Salary />
+        </div>
+        <div style={{ display: this.props.workStyle !== CONSTANTS.WORK_STYLE.REGULAR_EMPLOYEE ? '' : 'none' }}>
+          <Sales />
+        </div>
+      </section>
+    );
+  }
+}
 
-// const Revenue = (connect(mapStateToProps, mapDispatchToProps)(Salary));
-export default Revenue;
+const mapStateToProps = (state:any) => ({
+  workStyle: state.workStyle,
+});
+
+export default connect(mapStateToProps)(Revenue);
--- src/define.tsx
+++ src/define.tsx
@@ -1,11 +1,11 @@
 export const WORK_STYLE = {
   REGULAR_EMPLOYEE: 1,
   SELF_EMPLOYEE: 2,
-  OWN_COMPANY: 3,
+  MULTI: 3,
 };
 
 export const WORK_STYLE_NAME = new Map([
   [WORK_STYLE.REGULAR_EMPLOYEE, '社員'],
   [WORK_STYLE.SELF_EMPLOYEE, '個人事業主'],
-  [WORK_STYLE.OWN_COMPANY, '法人成り'],
+  [WORK_STYLE.MULTI, '両方'],
 ]);
